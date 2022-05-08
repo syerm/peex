c593e241f1a4d52e662099a0b8c617ae1ccc7e7a
Stanislav Yermolenko <syerm@softserveinc.com>
2022-05-08 16:47:52 +0300

    add security rules at new branch

--- main.tf
+++ main.tf
@@ -39,3 +39,24 @@ resource "azurerm_subnet" "subnet" {
   address_prefixes     = ["10.123.1.0/24"]
 
 }
+
+resource "azurerm_network_security_group" "security-group" {
+  name                = "syer-security-group"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+}
+
+resource "azurerm_network_security_rule" "security-rules" {
+  name                        = "syer-security-rules"
+  priority                    = 100
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "*"
+  source_port_range           = "*"
+  destination_port_range      = "*"
+  source_address_prefix       = "194.44.96.28/32"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.rg.name
+  network_security_group_name = azurerm_network_security_group.security-group.name
+} 
