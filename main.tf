0adae247d1cf60f95ba8d7a34b1d314224cdc2fc
Stanislav Yermolenko <syerm@softserveinc.com>
2022-05-08 15:49:52 +0300

    add virtual network

--- main.tf
+++ main.tf
@@ -22,3 +22,11 @@ resource "azurerm_resource_group" "rg" {
     Project    = "internal"
   }
 }
+
+resource "azurerm_virtual_network" "vn" {
+  name                = "syer-network"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  address_space       = ["10.123.0.0/16"]
+
+}
